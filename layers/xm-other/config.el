0ae45c6ee85a3bfce5df0d7cfa7b735ce930c1ff
Chen Xianmin <xianmin12@gmail.com>
2020-09-29 14:20:11 +0800

    修改 minor mode 在 modeline 中的显示

--- layers/xm-other/config.el
+++ layers/xm-other/config.el
@@ -9,3 +9,8 @@
 (global-set-key (kbd "M-C-9") '(lambda () (interactive) (adjust-opacity nil 10)))
 (global-set-key (kbd "M-C-0") '(lambda () (interactive) (modify-frame-parameters nil `((alpha . 100)))))
 
+;; 改变 minor mode 显示
+(spacemacs|diminish purpose-mode " P" " P")
+
+;; yasnippet 设置
+(setq yas-snippet-dirs '("~/.spacemacs.d/snippets/"))
